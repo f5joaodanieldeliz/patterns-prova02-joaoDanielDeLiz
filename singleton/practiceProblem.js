4afff8a7b0deacd0182a938bb2aa9c4e889f9cd8
Vinicius Teixeira Colombo <vinicius.296922@alunosatc.edu.br>
2025-09-10 20:20:47 -0300

    subindo

--- singleton/practiceProblem.js
+++ singleton/practiceProblem.js
@@ -1,9 +1,14 @@
-// Classe Database
 class Database {
   constructor(connectionString) {
+    if (Database.instance) {
+      return Database.instance;
+    }
+
     this.connectionString = connectionString;
     this.id = Math.random();
     console.log(`🔌 Nova conexão criada: ${this.id}`);
+    
+    Database.instance = this;
   }
 
   query(sql) {
@@ -11,9 +16,12 @@ class Database {
   }
 }
 
-// Cliente cria várias conexões (mesmo que não precise)
+console.log("--- Tentando criar a primeira conexão ---");
 const db1 = new Database("db://meu-banco");
 db1.query("SELECT * FROM users");
 
+console.log("\n--- Tentando criar a segunda conexão ---");
 const db2 = new Database("db://meu-banco");
 db2.query("SELECT * FROM products");
+
+console.log(`\ndb1 e db2 são a mesma instância? ${db1 === db2 ? "Sim ✅" : "Não ❌"}`);
