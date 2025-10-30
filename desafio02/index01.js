8576c8cbc2c49c2282395b0ab6c44864d3034ba8
Joao Daniel De Liz <joao.52321@alunosatc.edu.br>
2025-10-30 20:45:43 -0300

    feito

--- desafio02/index01.js
+++ desafio02/index01.js
@@ -1,1 +1,52 @@
-console.log("Hello, World! 001");
+class TaskSystemItem {
+    showDetails() {
+      throw new Error("Método abstrato!");
+    }
+  }
+
+
+class Task extends TaskSystemItem {
+    constructor(name) {
+      super();
+      this.name = name;
+    }
+  
+    showDetails() {
+      console.log(`Tarefa ${this.name}`);
+    }
+}
+  
+class Project extends TaskSystemItem {
+    constructor(name) {
+      super();
+      this.name = name;
+      this.tasks = [];
+    }
+  
+    add(task) {
+      this.tasks.push(task);
+    }
+  
+    showDetails(indent = "") {
+      console.log(`Projeto: ${this.name}`);
+      this.tasks.forEach((task) => task.showDetails(indent + "   "));
+    }
+}
+  
+  // Cliente
+  const t1 = new Task("Escrever documentação");
+  const t2 = new Task("Fazer testes");
+  const t3 = new Task("Fazer Refatura");
+
+  const Refutar = new Project("Refatura");
+  Refutar.add(t3)
+  
+  const p = new Project("Lançamento v1.0");
+
+ 
+  p.add(t1);
+  p.add(t2);
+  p.add(Refutar)
+  
+  p.showDetails();
+  
--- desafio02/index02.js
+++ desafio02/index02.js
@@ -1,1 +1,40 @@
-console.log("Hello, World! 002");
+class TV {
+    turnOn() {
+      console.log("TV ligada.");
+    }
+    turnOff() {
+      console.log("TV desligada.");
+    }
+}
+
+class Projetor {
+    turnOn() {
+      console.log("Projetor ligada.");
+    }
+    turnOff() {
+      console.log("Projetor desligada.");
+    }
+}
+  
+class RemoteControl {
+    constructor(Power) {
+      this.Power = Power;
+    }
+  
+    pressPowerButton(on) {
+      if (on) this.Power.turnOn();
+      else this.Power.turnOff();
+    }
+}
+
+  const tv = new TV()
+  const pro = new Projetor()
+  
+  const remote = new RemoteControl(tv);
+  const remoteP = new RemoteControl(pro);
+  remote.pressPowerButton(true);
+  remote.pressPowerButton(false);
+  remoteP.pressPowerButton(true);
+  remoteP.pressPowerButton(false);
+
+  
